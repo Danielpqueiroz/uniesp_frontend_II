80f251e8e2f5402b749f2bb22f7d0527d99bcc95
Daniel pontes de queiroz <Danielqueiroz@academicos.com>
2023-02-16 20:54:35 -0300

    Primeira aula

---
+++ aula01/aula01.js
@@ -0,0 +1,13 @@
+alunos = "Daniel"
+aluno = 10.0
+
+console.log("alunos");
+console.log("aluno");
+
+var numero1 = 10;
+var numero2 = "10";
+if (numero1 == numero2){
+    console.log("os valores são iguais.")
+}else{
+    console.log("Os valores são diferentes.")
+}
