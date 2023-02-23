bfe654844c391fb5b3d7cba5b8c70fa78e4fbdd7
Daniel pontes de queiroz <Danielqueiroz@academicos.com>
2023-02-23 20:21:02 -0300

    Função let

--- aula02/aula02.js
+++ aula02/aula02.js
@@ -1,1 +1,23 @@
 //Segunda aula
+var aluno = "João";
+
+console.log(aluno);
+//teste();
+console.log(teste())
+
+function teste() {
+    var aluno2 = "Pedro";
+    console.log(aluno2);
+    return aluno2;
+}
+
+
+if (aluno == "João"){
+    let resultado = "Olá João";
+    var resultado2 = "Olá João";
+}else {
+    let resultado = "Olá Desconhecido";
+    var resultado2 = "Olá Desconhecido";
+}
+//console.log(resultado);//O let só funciona dentro da função.Por isso não funciona aqui.
+console.log(resultado2)
