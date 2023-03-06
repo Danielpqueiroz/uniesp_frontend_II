895114f0e095622bdd8ef64192090c85ab982ce7
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2023-03-06 20:47:04 -0300

    atividade concluida

--- aula03.js/atividade01/atv01.js
+++ aula03.js/atividade01/atv01.js
@@ -1,5 +1,42 @@
-var nome=[]
-for (var i = 0; i<= 10; i++){
-var nome,[i] = prompt("Qual é o seu nome?");
-var altura,[i] = prompt("Digite sua altura?");
+var altura = [];
+var sexo = [];
+var soma = 0;
+var contador = 0;
+var qtdMulher = 0;
+var maiorAltura = 0;
+var menorAltura = 0;
+var resultado = 0;
+
+for (var i = 1; i<=15; i++){
+    altura[i]= parseFloat(prompt("Informe sua altura: Ex(1.70)"));    
+    sexo[i]= prompt("Informe seu sexo: Ex:(Digite M para masculino  e F para feminino)");    
+    
+}   
+
+maioraAltura = altura[1];
+menorAltura = altura[1];
+for (var i = 1; i<=15; i++){
+    
+    if (altura[i] > maiorAltura){
+        maiorAltura = altura[i];
+    } 
+    if (menorAltura > altura[i]) {
+        menorAltura = altura[i]
+    } 
+    
+
+    if (sexo[i] == "M" || sexo[i] == "m"){
+        soma = soma + altura[i];
+        contador = contador + 1;
+
+    }else if(sexo[i] == "F" || sexo[i] == "f"){
+        qtdMulher = qtdMulher + 1;
+    }else{
+        alert("ERRO")
+    }
 }
+
+resultado = soma/contador;
+
+var mensagem = "A maior altura é: " + maiorAltura + "\nA menor altura é: " + menorAltura + "\nA media da altura dos homens é: " + resultado.toFixed(2) + "\nA quantidade de mulheres é: " + qtdMulher;
+alert(mensagem);
--- aula03.js/aula03.js
+++ aula03.js/aula03.js
@@ -1,4 +1,4 @@
-//var nome = "Daniel";
-//alert(nome);
+var nome = "Daniel";
+alert(nome);
 var novoTexto ="O titulo foi alterado via JS";
 document.getElementById("Titulo").innerHTML = novoTexto;
