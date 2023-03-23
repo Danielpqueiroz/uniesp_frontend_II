091c91fca7b600f07d8c4547ff3351ad3c81bdbe
Daniel Pontes <2022211510026@iesp.edu.br>
2023-03-23 20:20:15 -0300

    Update funcao.js

--- aula05/funcao.js
+++ aula05/funcao.js
@@ -6,7 +6,7 @@ function somar(numero1, numero2){
 var resultado = somar(1,5);
 console.log(resultado);
 
-
+//Função
 function boasVindas(){
     console.log("-------------SISTEMA TAL  ----------");
     console.log("Seja bem vindo, quinta-feira!");
@@ -14,12 +14,9 @@ function boasVindas(){
 }
 
 boasVindas();
-
+//Declaração de função
 var multiplicar = function(n1,n2){
     return (n1*n2);
 }
 
 console.log(multiplicar(2,6));
-
-
-
