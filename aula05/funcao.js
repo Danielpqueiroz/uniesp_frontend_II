3da2031997b6b0846ae0668e539cb0068665836a
Daniel pontes de queiroz <Danielqueiroz@academicos.com>
2023-03-23 20:01:47 -0300

    aula05

---
+++ aula05/funcao.js
@@ -0,0 +1,25 @@
+function somar(numero1, numero2){
+    var total = numero1+numero2;
+    return total;
+}
+
+var resultado = somar(1,5);
+console.log(resultado);
+
+
+function boasVindas(){
+    console.log("-------------SISTEMA TAL  ----------");
+    console.log("Seja bem vindo, quinta-feira!");
+    console.log("Versão do sistema: tal.");
+}
+
+boasVindas();
+
+var multiplicar = function(n1,n2){
+    return (n1*n2);
+}
+
+console.log(multiplicar(2,6));
+
+
+
