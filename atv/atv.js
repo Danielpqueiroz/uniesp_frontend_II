0e69a2c0e49e4c40fba5abcf386d3b51ef96310c
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2023-03-30 15:56:50 -0300

    atv

--- atv/atv.js
+++ atv/atv.js
@@ -1,8 +1,50 @@
 var n1 = 2;
-
-maiorIgual();
-
 function maiorIgual (n1){
-    if (n1 >= 0 && n1 % 0) console.log ("Esse numero é par");
+    if (n1 >= 0 && n1 % 2 == 0) {
+        console.log ("Esse numero é par")
+    }
+    if (n1 >= 0 && n1 % 2 == 1) {
+        console.log ("Esse numero é impar")
+    }
+    if (n1 < 0){
+        console.log("É obrigatorio informar um numero maior igual a zero.")
+    }
 
+}
+
+maiorIgual(n1);
+
+
+var numeros = [1,2,3];
+
+/*function multiplicado(){
+    for (var i = 0 ; numeros.length ; i++){
+        console.log(numeros[i]*2);
+    }
+}*/
+if (numeros.length === 0){
+    console.log("É obrigatorio informar pelo menos um numero.")
+}else{
+    numeros.forEach(function(elemento){
+        console.log(elemento);
+    });//Percorre a lista mostrando os valores
+}
+
+
+var array = [1,2,3,4];
+
+if (array.length === 0){
+    console.log("É obrigatorio informar pelo menos um numero.")
+}else{
+    array.forEach(function(elemento){
+        console.log(elemento);
+    });//Percorre a lista mostrando os valores
+    
+    var num2 = array.map(function(elemento){
+        return elemento*2;
+    });//Percorre o array mutiplicando por 2 gerando um novo array
+    
+    num2.forEach(function(elemento){
+        console.log(elemento);
+    });//Percorre a lista mostrando os valores
 }
